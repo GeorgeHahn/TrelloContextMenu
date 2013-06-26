3b28f5a305e788df964acaf61ce29ba139a7e3be
George Hahn <george.hahn.vhs@gmail.com>
2013-06-26 02:12:42 -0400

    Moved context menu interface to its own file

---
+++ TrelloContextMenu/ITestableContextMenu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace TrelloContextMenu
+{
+    public interface ITestableContextMenu
+    {
+        bool CanShowMenu(IEnumerable<string> selectedItemPaths, string folderPath);
+        ToolStripMenuItem CreateMenuItem(Func<IEnumerable<string>> selectedItemPaths, string folderPath);
+    }
+}
